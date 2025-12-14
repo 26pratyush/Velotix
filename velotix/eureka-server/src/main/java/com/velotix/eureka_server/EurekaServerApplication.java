6bbc7aeb53f2b060227c35ebd4f610e18e16b687
prat <paipratyush1@gmail.com>
2025-12-14 12:44:32 +0530

    Updated API Gateway and added new configuration files

--- velotix/eureka-server/src/main/java/com/velotix/eureka_server/EurekaServerApplication.java
+++ velotix/eureka-server/src/main/java/com/velotix/eureka_server/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.velotix.eurekaserver;
+package com.velotix.eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
